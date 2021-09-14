f9abe0bc3bfcf8bb9352ef2706d77644fdede5d2
Quinlan Ragsdale <qragsda80@ccs.local>
2021-09-14 10:08:08 -0400

    adding capacitive touch

--- servo.py
+++ servo.py
@@ -1,19 +1,31 @@
-"""CircuitPython Essentials Servo standard servo example"""
 import time
 import board
-import pwmio
+import pulseio
+import touchio
 import servo
 
 # create a PWMOut object on Pin A2.
-pwm = pwmio.PWMOut(board.A2, duty_cycle=2 ** 15, frequency=50)
+pwm = pulseio.PWMOut(board.A2, duty_cycle=2 ** 15, frequency=50)
 
 # Create a servo object, my_servo.
 my_servo = servo.Servo(pwm)
 
+touch_pad1 = board.A0  # Will not work for Circuit Playground Express!
+touch1 = touchio.TouchIn(touch_pad1)
+touch_pad2 = board.A5  # Will not work for Circuit Playground Express!
+touch2 = touchio.TouchIn(touch_pad2)
+
 while True:
-    for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time.
-        my_servo.angle = angle
-        time.sleep(0.05)
-    for angle in range(180, 0, -5): # 180 - 0 degrees, 5 degrees at a time.
-        my_servo.angle = angle
-        time.sleep(0.05)
+
+    if touch1.value:
+        print("Touched the White Wire!")
+        for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time.
+            my_servo.angle = angle
+            time.sleep(0.05)
+    if touch2.value:
+        print("Touched the Green Wire!")
+        for angle in range(180, 0, -5):  # 180 - 0 degrees, 5 degrees at a time
+            my_servo.angle = angle
+            time.sleep(0.05)
+    time.sleep(0.05)
+    print("end of loop!")
